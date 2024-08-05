25be72159f6818adda81991c99a01b905a47dbf3
harold <haroldpeter6@gmail.com>
2024-08-05 12:34:05 +0300

    Android build

--- src/app/services/env.service.ts
+++ src/app/services/env.service.ts
@@ -4,9 +4,9 @@ import { Injectable } from '@angular/core';
 })
 export class EnvService {
 	/*Live deploy: uncomment this before build*/
-  // API_URL = 'http://silverms.com/api/';
+  API_URL = 'http://172.105.78.107/api/';
   
   //Localhost
-  API_URL = 'http://127.0.0.1:8000/api/';
+  // API_URL = 'http://127.0.0.1:8000/api/';
   constructor() { }
 }
